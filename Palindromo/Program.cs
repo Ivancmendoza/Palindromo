f92293ac9536a1efb061612191d18d9ed3b2dac0
ivancmendoza <ivan.cmendoza.2020@gmail.com>
2022-08-08 13:33:47 -0500

    Agregar archivos de proyecto.

---
+++ Palindromo/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string text = "";
+Console.WriteLine("Escribe algo");
+text = Console.ReadLine();
+char[] charArray = text.ToCharArray();
+string reverse = String.Empty;
+for (int i = charArray.Length - 1; i >= 0; i--)
+{
+    reverse += charArray[i];
+}
+if (reverse == text)
+{
+    Console.WriteLine("Este texto es un palindromo");
+}
+else
+{
+    Console.WriteLine("Este texto no es un palindromo");
+}
+
