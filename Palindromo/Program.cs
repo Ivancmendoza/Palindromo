c1ba191ef93d9adb56571c9ec624de5fbcdc360e
Ivancmendoza <110744750+Ivancmendoza@users.noreply.github.com>
2022-08-08 16:38:35 -0500

    Update Program.cs

--- Palindromo/Program.cs
+++ Palindromo/Program.cs
@@ -1,19 +1,21 @@
-﻿
-string text = "";
+
+using System.Text.RegularExpressions;
+
+string Text = "";
 Console.WriteLine("Escribe algo");
-text = Console.ReadLine();
-char[] charArray = text.ToCharArray();
+Text = Console.ReadLine();
+string newText = Regex.Replace(Text, @"\s", "");
+char[] charArray = newText.ToCharArray();
 string reverse = String.Empty;
 for (int i = charArray.Length - 1; i >= 0; i--)
 {
     reverse += charArray[i];
 }
-if (reverse == text)
+if (reverse == newText)
 {
-    Console.WriteLine("Este texto es un palindromo");
+    Console.WriteLine("{0} SI es un palindromo", Text);
 }
 else
 {
-    Console.WriteLine("Este texto no es un palindromo");
+    Console.WriteLine("{0} NO es un palindromo", Text);
 }
-
